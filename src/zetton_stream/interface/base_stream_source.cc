35b150d1919774c441e5ad04c3c78d52b0359c9d
corenel <xxdsox@gmail.com>
2022-09-13 20:32:52 +0800

    style: add pre-commit

--- src/zetton_stream/interface/base_stream_source.cc
+++ src/zetton_stream/interface/base_stream_source.cc
@@ -7,29 +7,27 @@ namespace stream {
 
 const char *StreamSourceTypeToStr(StreamSourceType type) {
   switch (type) {
-  case StreamSourceType::SOURCE_UNKNOWN:
-    return "unknown";
-  case StreamSourceType::SOURCE_CAMERA:
-    return "camera";
-  case StreamSourceType::SOURCE_VIDEO:
-    return "video";
-  case StreamSourceType::SOURCE_IMAGE:
-    return "image";
-  default:
-    return "unknown";
+    case StreamSourceType::SOURCE_UNKNOWN:
+      return "unknown";
+    case StreamSourceType::SOURCE_CAMERA:
+      return "camera";
+    case StreamSourceType::SOURCE_VIDEO:
+      return "video";
+    case StreamSourceType::SOURCE_IMAGE:
+      return "image";
+    default:
+      return "unknown";
   }
 }
 
 StreamSourceType StreamSourceTypeFromStr(const char *str) {
-  if (!str)
-    return StreamSourceType::SOURCE_UNKNOWN;
+  if (!str) return StreamSourceType::SOURCE_UNKNOWN;
   for (int n = 0; n < StreamSourceType::SOURCE_MAX_NUM; n++) {
-    const StreamSourceType value = (StreamSourceType)n;
-    if (strcasecmp(str, StreamSourceTypeToStr(value)) == 0)
-      return value;
+    const auto value = (StreamSourceType)n;
+    if (strcasecmp(str, StreamSourceTypeToStr(value)) == 0) return value;
   }
   return StreamSourceType::SOURCE_UNKNOWN;
 }
 
-} // namespace stream
-} // namespace zetton
+}  // namespace stream
+}  // namespace zetton
